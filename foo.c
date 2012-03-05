ff3caff61c9587522d4f617179369283a558cfef
Jason Reed <jcreed@gmail.com>
2012-03-04 21:38:00 -0500

    v. crunchy

--- foo.c
+++ foo.c
@@ -13,23 +13,90 @@
 jack_client_t *jack_client = NULL;
 jack_port_t *output_port[MAX_JACK_PORTS];
 jack_default_audio_sample_t mybuf[MYBUF_SIZE];
+jack_default_audio_sample_t passbuf[MYBUF_SIZE];
 #define randf() (rand() / (float)MAX_INT)
+float lastPassbuf = 0;
 
 int sample_rate = 48000;
 float f = 440.0;
-float tt = 0.0;
+
+typedef struct {
+  float f;
+  float tt;
+  int pitch;
+  int active;
+} osc_t;
+
+#define NUM_OSCS 16
+osc_t oscs[NUM_OSCS];
+
+void init_oscs() {
+  int i;
+  for (i = 0; i < NUM_OSCS; i++) {
+    oscs[i].active = 0;
+  }
+}
+
+void new_osc(int pitch) {
+  int i;
+
+  for (i = 0; i < NUM_OSCS; i++) {
+    if (!oscs[i].active) {
+      oscs[i].active = 1;
+      oscs[i].tt = 0.0;
+      oscs[i].f = 440.0 * pow(2.0, (pitch - 69) / 12.0);
+      oscs[i].pitch = pitch;
+      return;
+    }
+  }
+  // if we get here, no more polyphony left!
+}
+
+void del_osc(int pitch) {
+  int i;
+
+  for (i = 0; i < NUM_OSCS; i++) {
+    if (oscs[i].pitch == pitch) {
+      oscs[i].active = 0;
+    }
+  }
+}
 
 static int
 process (jack_nframes_t nframes, void *arg) {
-  int i;
-  for (i = 0; i < nframes; i++) {
-    tt += f / sample_rate;
-    if (tt > 1.0) tt -= 1.0;
-    mybuf[i] = sin(2 * PI * tt) * 0.1;
+  int i, j;
+  memset(mybuf, 0, nframes * sizeof(jack_default_audio_sample_t));
+  for (i = 0; i < NUM_OSCS; i++) {
+    if (!oscs[i].active) continue;
+    osc_t* this = &oscs[i];
+    float f = this->f;
+    for (j = 0; j < nframes; j++) {
+      float tt = this->tt;
+      tt += 1.0 / sample_rate;
+      mybuf[j] += sin(2 * PI * f * tt) ;
+      this->tt = tt;
+    }
   }
+
+#define SMOOTH 0.005
+  float diff = mybuf[0] - lastPassbuf;
+    if (fabs(diff) > SMOOTH) {
+       passbuf[0] = lastPassbuf + SMOOTH * (diff > 0 ? 1 : -1);
+    }
+    else passbuf[0] = mybuf[0];
+
+  for (i = 1; i < nframes; i++) {
+    float diff = 2 * mybuf[i] - passbuf[i-1];
+    if (fabs(diff) > SMOOTH) {
+       passbuf[i] = passbuf[i-1] + SMOOTH * (diff > 0 ? 1 : -1);
+    }
+    else passbuf[i] = 2 * mybuf[i];
+  }
+
+  lastPassbuf = passbuf[nframes-1];
   for (i = 0; i < 2; i++) {
     jack_default_audio_sample_t *out = jack_port_get_buffer(output_port[i], nframes);
-    memcpy(out, mybuf, nframes * sizeof(jack_default_audio_sample_t));
+    memcpy(out, passbuf, nframes * sizeof(jack_default_audio_sample_t));
   }
 }
 
@@ -111,9 +178,7 @@ int main(void) {
 	if (ret <= 0)
 	  fprintf(stderr, "Midi read error\n");
 	else printf("%d %d\n", c[1], c[2]);
-	if (c[2]) {
-	  f = 440.0 * pow(2.0, (c[1] - 69) / 12.0);
-	}
+	if (c[2]) new_osc(c[1]); else del_osc(c[1]);
 	//	printf("connected? %d\n", jack_port_connected_to(output_port[0], "system:playback_1"));
 	did = 1;
       }
